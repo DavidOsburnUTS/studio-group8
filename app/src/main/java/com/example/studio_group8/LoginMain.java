adacb71ab3514055dd5d29727cbec4e30e6dfd47
David Osburn <david.osburn@student.uts.edu.au>
2019-08-30 13:07:40 +1000

    added firebase authentication, nothing hooked up yet.

--- app/src/main/java/com/example/studio_group8/LoginMain.java
+++ app/src/main/java/com/example/studio_group8/LoginMain.java
@@ -2,13 +2,19 @@ package com.example.studio_group8;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginMain extends AppCompatActivity {
+
+    FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-    }
 
+        mAuth = FirebaseAuth.getInstance();
+
+}
 
 }
