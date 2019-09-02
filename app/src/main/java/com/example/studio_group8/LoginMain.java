cbc68bc73e6966bdbee8f049c3babc25cef55384
wasif <wasif.hossain@hotmail.com>
2019-09-02 20:51:07 +1000

    #Updated Login with animations and permissions

--- app/src/main/java/com/example/studio_group8/LoginMain.java
+++ app/src/main/java/com/example/studio_group8/LoginMain.java
@@ -1,11 +1,34 @@
 package com.example.studio_group8;
 
+import android.Manifest;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 //import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginMain extends AppCompatActivity {
 
+    Button loginBtn;
+
+    static final int requestcode = 1;
+    Animation rightleft;
+    LinearLayout L1;
+    Animation bottomup;
+    Animation topdown;
+    RelativeLayout R1;
+    RelativeLayout R2;
 //    FirebaseAuth mAuth;
 
     @Override
@@ -13,8 +36,77 @@ public class LoginMain extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+
+        R1 = (RelativeLayout) findViewById(R.id.RelativeLayout);
+        R2 = (RelativeLayout) findViewById(R.id.RelativeLayout2);
+        L1 = (LinearLayout) findViewById(R.id.Layout1);
+
+        rightleft = AnimationUtils.loadAnimation(this, R.anim.righttoleft);
+        bottomup = AnimationUtils.loadAnimation(this,R.anim.bottomup);
+        topdown = AnimationUtils.loadAnimation(this,R.anim.topdown);
+
+        R1.setAnimation(bottomup);
+        R2.setAnimation(topdown);
+        L1.setAnimation(rightleft);
+
+        checkPermission();
+
+
+
+
+
 //        mAuth = FirebaseAuth.getInstance();
 
 }
+
+    public void checkPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                //PERMISSION GRANTED
+            } else {
+                ActivityCompat.requestPermissions(LoginMain.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+
+            }
+        } else {
+            Toast.makeText(getApplicationContext(), "Permission Granted", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (grantResults[0] == PackageManager.PERMISSION_GRANTED && requestCode == LoginMain.requestcode) {
+            Toast.makeText(getApplicationContext(), "Permission Granted", Toast.LENGTH_SHORT).show();
+        } else {
+            showDetails();
+
+        }
+    }
+
+    public void showDetails() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(LoginMain.this);
+        builder.setTitle("Storage  Write Permission")
+                .setMessage("This permission is necessary to access storage to be able to save data")
+                .setPositiveButton(android.R.string.ok, new Dialog.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                            ActivityCompat.requestPermissions(LoginMain.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+                        }
+                    }
+                })
+                .setNegativeButton(android.R.string.cancel, new Dialog.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                });
+        builder.create();
+        builder.show();
+
+
+    }
+
 
 }
