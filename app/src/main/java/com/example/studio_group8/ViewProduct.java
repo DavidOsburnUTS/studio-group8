5b0948a17efb9edaa5461e9b29437dab94943c5c
Wasif <wasif.hossain@hotmail.com>
2019-10-04 14:42:22 +1000

    #Added ViewProduct Class and Layout

---
+++ app/src/main/java/com/example/studio_group8/ViewProduct.java
@@ -0,0 +1,13 @@
+package com.example.studio_group8;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ViewProduct  extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_view);
+    }
+}
