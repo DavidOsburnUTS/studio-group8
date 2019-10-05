168753ca1e34cf05c4dbfbbf43dd9d48b9c13a3d
Akshay <dhamakk@gmail.com>
2019-10-06 10:50:31 +1100

    Added add image fuctionality to add product

--- app/src/main/java/com/example/studio_group8/SellProduct.java
+++ app/src/main/java/com/example/studio_group8/SellProduct.java
@@ -1,6 +1,11 @@
 package com.example.studio_group8;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -9,13 +14,23 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class SellProduct extends AppCompatActivity {
 
@@ -24,7 +39,14 @@ public class SellProduct extends AppCompatActivity {
     private Button mupload;
     private int mquint =1;
     private String selectedItemText, image = "123aa";
-//    int quantity = 1;
+    public static final int GET_FROM_GALLERY = 3;
+//  for image
+private Uri image_uri;
+    public Bitmap imag;
+    private FirebaseStorage storage = FirebaseStorage.getInstance();
+    private StorageReference storageRef = storage.getReference();
+    private String download_uri;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,6 +61,7 @@ public class SellProduct extends AppCompatActivity {
         /*String value = mquantity.getText().toString();
         mquint =Integer.parseInt(value);*/
         final int mquint = Integer.parseInt(mquantity.getText().toString().trim());
+
         mupload.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -108,8 +131,61 @@ public class SellProduct extends AppCompatActivity {
             DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
             mDatabase.child("Product").child(addProduct.name).setValue(addProduct);
 
-            Toast.makeText(this, "Number" + mquint + selectedItemText , Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "added successfully"+image , Toast.LENGTH_SHORT).show();
         }
 
+    }
+
+    public void getImage(View view) {
+
+        startActivityForResult(new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.INTERNAL_CONTENT_URI), GET_FROM_GALLERY);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        //Detects request codes
+        if (requestCode == GET_FROM_GALLERY && resultCode == Activity.RESULT_OK) {
+            image_uri = data.getData();
+            image = null;
+            try {
+                imag = MediaStore.Images.Media.getBitmap(this.getContentResolver(), image_uri);
+
+                final StorageReference eventRef = storageRef.child(image_uri.getLastPathSegment());
+                UploadTask uploadTask = eventRef.putFile(image_uri);
+
+                uploadTask.addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception exception) {
+                        // Handle unsuccessful uploads
+                    }
+                }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        // taskSnapshot.getMetadata() contains file metadata such as size, content-type, and download URL.
+                        Task<Uri> downloadUrl = taskSnapshot.getMetadata().getReference().getDownloadUrl();
+                        downloadUrl.addOnSuccessListener(new OnSuccessListener<Uri>() {
+                            @Override
+                            public void onSuccess(Uri uri) {
+
+                                download_uri = uri.toString();
+                                image = download_uri;
+
+                            }
+                        });
+
+                    }
+                });
+
+            } catch (FileNotFoundException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
     }
 }
