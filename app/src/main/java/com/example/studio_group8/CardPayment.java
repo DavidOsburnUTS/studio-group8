c357d809a2aea24ac891ff94cf1228ca9933cede
David Osburn <david.osburn@student.uts.edu.au>
2019-10-09 14:19:19 +1100

    credit card form created using CardForm API

---
+++ app/src/main/java/com/example/studio_group8/CardPayment.java
@@ -0,0 +1,76 @@
+package com.example.studio_group8;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import android.app.AlertDialog.Builder;
+
+
+import com.braintreepayments.cardform.view.CardForm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CardPayment extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment);
+
+        CardForm cardForm = findViewById(R.id.card_form);
+        Button buy = findViewById(R.id.btnBuy);
+
+        cardForm.cardRequired(true)
+                .expirationRequired(true)
+                .cvvRequired(true)
+                .postalCodeRequired(true)
+                .mobileNumberRequired(true)
+                .mobileNumberExplanation("SMS is required on this number")
+                .setup(CardPayment.this);
+
+        cardForm.getCvvEditText().setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_VARIATION_PASSWORD);
+
+        buy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (cardForm.isValid()) {
+                    AlertDialog.Builder alertBuilder = new AlertDialog.Builder(CardPayment.this);
+                    alertBuilder.setTitle("Confirm before purchase");
+                    alertBuilder.setMessage("Card number: " + cardForm.getCardNumber() + "\n" +
+                            "Card expiry date: " + cardForm.getExpirationDateEditText().getText().toString() + "\n" +
+                            "Card CVV: " + cardForm.getCvv() + "\n" +
+                            "Postal code: " + cardForm.getPostalCode() + "\n" +
+                            "Phone number: " + cardForm.getMobileNumber());
+                    alertBuilder.setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            dialogInterface.dismiss();
+                            Toast.makeText(CardPayment.this, "Thank you for purchase", Toast.LENGTH_LONG).show();
+                        }
+                    });
+                    alertBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            dialogInterface.dismiss();
+                        }
+                    });
+                    AlertDialog alertDialog = alertBuilder.create();
+                    alertDialog.show();
+
+                }else {
+                    Toast.makeText(CardPayment.this, "Please complete the form", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+    }
+
+    public void back( View view) {
+        finish();
+    }
+}
--- app/src/main/java/com/example/studio_group8/CartActivity.java
+++ app/src/main/java/com/example/studio_group8/CartActivity.java
@@ -1,12 +1,14 @@
 package com.example.studio_group8;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.app.Activity;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class CartActivity extends AppCompatActivity {
+public class CartActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,8 +17,10 @@ public class CartActivity extends AppCompatActivity {
 
     }
 
-    public void confirmAndPay(View view) {
-
+    public void confirmAndPay (View view) {
+        Intent cardpayment = new Intent(CartActivity.this, CardPayment.class);
+        startActivity(cardpayment);
+        overridePendingTransition( R.anim.right_slide_in, R.anim.left_slide_out);
     }
 
     public void back( View view) {
