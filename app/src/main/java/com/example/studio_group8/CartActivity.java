ee3d4df36947a4aaaa3a3f8939c9725a71db8ddc
David Osburn <david.osburn@student.uts.edu.au>
2019-10-09 13:27:53 +1100

    Card Form dependency added, onClick added for checkout btn

--- app/src/main/java/com/example/studio_group8/CartActivity.java
+++ app/src/main/java/com/example/studio_group8/CartActivity.java
@@ -2,6 +2,7 @@ package com.example.studio_group8;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,6 +12,10 @@ public class CartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_checkout);
+
+    }
+
+    public void confirmAndPay(View view) {
 
     }
 
