57a759eea1bdefcb4f00986ebb97ed597bad0958
Akshay <dhamakk@gmail.com>
2019-10-15 19:51:07 +1100

    New order method with some issues

--- app/src/main/java/com/example/studio_group8/CardPayment.java
+++ app/src/main/java/com/example/studio_group8/CardPayment.java
@@ -7,16 +7,20 @@ import android.os.Bundle;
 import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.app.AlertDialog.Builder;
 
 
 import com.braintreepayments.cardform.view.CardForm;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.GenericTypeIndicator;
 import com.google.firebase.database.ServerValue;
 import com.google.firebase.database.ValueEventListener;
 
@@ -25,12 +29,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.UUID;
 
 public class CardPayment extends AppCompatActivity {
 // cart number 5234269538465723
 String orderid;
 private DatabaseReference cartListRef;
+    final String currentuser = FirebaseAuth.getInstance().getCurrentUser().getUid();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,7 +73,13 @@ private DatabaseReference cartListRef;
                             dialogInterface.dismiss();
 
                             Toast.makeText(CardPayment.this, "Thank you for purchase", Toast.LENGTH_SHORT).show();
-                            CreateOrder();
+                           // CreateOrder();
+                            GetProduct();
+                            final String user = FirebaseAuth.getInstance().getCurrentUser().getUid();
+                            cartListRef.child(user)
+                                    .child("Products")
+                                    .removeValue();
+
                             Intent home = new Intent(CardPayment.this, MainActivity.class);
                             startActivity(home);
                         }
@@ -93,56 +105,96 @@ private DatabaseReference cartListRef;
         finish();
     }
 
-    public void CreateOrder() {
-        final String currentuser = FirebaseAuth.getInstance().getCurrentUser().getUid();
+//    public void CreateOrder() {
+//        orderid = UUID.randomUUID().toString();
+//        Date c = Calendar.getInstance().getTime();
+//
+//
+//        SimpleDateFormat df = new SimpleDateFormat("dd-MM-yyyy");
+//        String formattedDate = df.format(c);
+//        cartListRef = FirebaseDatabase.getInstance().getReference().child("Cart");
+//        DatabaseReference fromPath = cartListRef.child(currentuser).child("Products");
+//        DatabaseReference ordersave = FirebaseDatabase.getInstance().getReference();
+//        DatabaseReference toPath = ordersave.child("Order").child(currentuser).child("Purchase date "+formattedDate).child(orderid);
+//        //AddOrder(fromPath, toPath);
+//
+//
+//
+//
+//
+//    }
+//
+//    private void AddOrder(final DatabaseReference fromPath, final DatabaseReference toPath) {
+//        fromPath.addListenerForSingleValueEvent(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(DataSnapshot dataSnapshot) {
+//                toPath.setValue(dataSnapshot.getValue(), new DatabaseReference.CompletionListener() {
+//                    @Override
+//                    public void onComplete(DatabaseError firebaseError, DatabaseReference firebase) {
+//                        if (firebaseError != null) {
+//                            System.out.println("Copy failed");
+//                        } else {
+//                            System.out.println("Success");
+//                            final String user = FirebaseAuth.getInstance().getCurrentUser().getUid();
+//                            cartListRef.child(user)
+//                                    .child("Products")
+//                                    .removeValue();
+//
+//                        }
+//                    }
+//                });
+//
+//            }
+//
+//            @Override
+//            public void onCancelled(DatabaseError databaseError) {
+//
+//            }
+//        });
+//    }
+
+    private void GetProduct(){
         orderid = UUID.randomUUID().toString();
         Date c = Calendar.getInstance().getTime();
 
+        DatabaseReference cartRef = FirebaseDatabase.getInstance().getReference().child("Cart");
 
         SimpleDateFormat df = new SimpleDateFormat("dd-MM-yyyy");
-        String formattedDate = df.format(c);
-        cartListRef = FirebaseDatabase.getInstance().getReference().child("Cart");
-        DatabaseReference fromPath = cartListRef.child(currentuser).child("Products");
-        DatabaseReference ordersave = FirebaseDatabase.getInstance().getReference();
-        DatabaseReference toPath = ordersave.child("Order").child(currentuser).child("Purchase date "+formattedDate).child(orderid);
-        AddOrder(fromPath, toPath);
+        String date = df.format(c);
 
-
-
-
-    }
-
-    private void AddOrder(final DatabaseReference fromPath, final DatabaseReference toPath) {
+        DatabaseReference fromPath = cartRef.child(currentuser).child("Products");
+        FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
         fromPath.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                toPath.setValue(dataSnapshot.getValue(), new DatabaseReference.CompletionListener() {
-                    @Override
-                    public void onComplete(DatabaseError firebaseError, DatabaseReference firebase) {
-                        if (firebaseError != null) {
-                            System.out.println("Copy failed");
-                        } else {
-                            System.out.println("Success");
-                            final String user = FirebaseAuth.getInstance().getCurrentUser().getUid();
-                            cartListRef.child(user)
-                                    .child("Products")
-                                    .removeValue();
-
-                        }
+                GenericTypeIndicator<HashMap<String, Product>> t = new GenericTypeIndicator<HashMap<String, Product>>(){};
+                HashMap<String, Product> dataset = dataSnapshot.getValue(t);
+                if (dataset != null) {
+                    for( String id : dataset.keySet() ){
+                      String productid = dataset.get(id).getproductid();
+                      String productname = dataset.get(id).getName();
+                      String imageurl = dataset.get(id).getImage();
+                      int quentity = dataset.get(id).getQuantity();
+                      
+                      AddOrder(orderid, productid, productname, imageurl, quentity, date);
+                        
                     }
-                });
+                }};
 
-            }
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+                }
 
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
 
-            }
-        });
-    }
 
-    private void IsertOrder(){
-
-    }
+    });
 
 }
+
+    private void AddOrder(String orderid, String productid, String productname, String imageurl, int quentity, String date) {
+
+        final Orderproduct orderproduct = new Orderproduct(orderid, productid, productname, imageurl, quentity, date);
+        DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
+        mDatabase.child("Neworder").child(orderid).setValue(orderproduct);
+    }
+    }
--- app/src/main/java/com/example/studio_group8/Orderdate.java
+++ app/src/main/java/com/example/studio_group8/Orderdate.java
@@ -1,10 +1,14 @@
 package com.example.studio_group8;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class Orderdate {
     public String date;
+
    private DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference().child("Order");
 
     public Orderdate() {
@@ -13,6 +17,7 @@ public class Orderdate {
 
     public Orderdate(String date){
         this.date = date;
+
     }
 
     public void setdate(String date) {
@@ -20,6 +25,8 @@ public class Orderdate {
     }
 
     public String getdate() {
+
+
 
         return date = mDatabase.push().getKey();
     }
